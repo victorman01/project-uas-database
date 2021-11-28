aa972c6558bdc6731412d894fb7239332c1eb166
Victor Manuel Soesilo <79180895+victorman01@users.noreply.github.com>
2021-11-28 17:15:15 +0700

    Fixing Seen Sent Chat dan ngerubah nama file

---
+++ SubrataSquad/OnlineMart_SubrataSquad/OnlineMart_LIB/Chat.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace OnlineMart_LIB
+{
+    public class Chat
+    {
+        private Pelanggan pelanggan;
+        private Driver driver;
+        private List<string> listChat;
+        private List<string> listStatus;
+        private string pengirim;
+
+        public Chat(Pelanggan pelanggan, Driver driver)
+        {
+            this.Pelanggan = pelanggan;
+            this.Driver = driver;
+            this.ListChat = new List<string>();
+            this.listStatus = new List<string>();
+            this.Pengirim = "";
+        }
+
+        public Pelanggan Pelanggan { get => pelanggan; set => pelanggan = value; }
+        public Driver Driver { get => driver; set => driver = value; }
+        public List<string> ListChat { get => listChat; set => listChat = value; }
+        public List<string> ListStatus { get => listStatus; set => listStatus = value; }
+        public string Pengirim { get => pengirim; set => pengirim = value; }
+
+        public void BacaPesan()
+        {
+            string sql = "";
+            sql = "select c.tanggal, concat(date_format( c.tanggal, '[%d/%m, %H:%i] ')" + " " + ", p.nama,' : ', c.pesan) as pesan" +
+                       " from chats c " +
+                       " inner join pelanggans p on c.pelanggan = p.id " +
+                       " inner join drivers d on d.id = c.driver " +
+                       " where c.pelanggan = " + Pelanggan.Id + " and c.driver = " + Driver.Id + " and c.pengirim = "+"'Pelanggan'"+
+                       " union" +
+                       " select c.tanggal, concat(date_format(c.tanggal, '[%d/%m, %H:%i] ')" + " " + ", d.nama,' : ', c.pesan) as pesan" +
+                       " from chats c " +
+                       " inner join pelanggans p on c.pelanggan = p.id " +
+                       " inner join drivers d on d.id = c.driver " +
+                       " where c.pelanggan = " + Pelanggan.Id + " and c.driver = " + Driver.Id + " and c.pengirim = " + "'Driver'" +
+                       " order by tanggal";
+
+            MySqlDataReader hasil = Connection.JalankanPerintahQuery(sql);
+            while (hasil.Read())
+            {
+                ListChat.Add(hasil.GetString(1));
+            }
+        }
+        public int TambahPesan(string chat, string pengirim)
+        {
+            string sql = "";
+            sql = "Insert into chats(tanggal, pelanggan,driver, pesan, pengirim) values('" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "'," + Pelanggan.Id + "," + Driver.Id + ",'" + chat + "','"+ pengirim + "')";
+            int hasil = Connection.JalankanPerintahDML(sql);
+            return hasil;
+        }
+        public void UpdateData(string penerima)
+        {
+            string sql = "Update chats set status = 'Seen' where pengirim = '"+ penerima + "' and driver = "+Driver.Id + " and pelanggan = "+ Pelanggan.Id;
+            Connection.JalankanPerintahDML(sql);
+        }
+        public void BacaStatus(string pengirim)
+        {
+            string sql = "";
+            sql = "select c.status " +
+                       "from chats c " +
+                       "inner join pelanggans p on c.pelanggan = p.id " +
+                       "inner join drivers d on d.id = c.driver " +
+                       " where c.pelanggan = " + Pelanggan.Id + " and c.driver = " + Driver.Id + " and c.pengirim = " + "'" + pengirim + "'";
+
+            MySqlDataReader hasil = Connection.JalankanPerintahQuery(sql);
+            while (hasil.Read())
+            {
+                ListStatus.Add(hasil.GetString(0));
+            }
+        }
+    }
+}
---
+++ SubrataSquad/OnlineMart_SubrataSquad/OnlineMart_SubrataSquad/FormChat.designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace OnlineMart_SubrataSquad
+{
+    partial class FormChat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonKirimChat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonKeluar = new System.Windows.Forms.Button();
+            this.listBoxChat = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.labelSeen = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonKirimChat
+            // 
+            this.buttonKirimChat.BackColor = System.Drawing.Color.Orange;
+            this.buttonKirimChat.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKirimChat.ForeColor = System.Drawing.Color.White;
+            this.buttonKirimChat.Location = new System.Drawing.Point(404, 5);
+            this.buttonKirimChat.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonKirimChat.Name = "buttonKirimChat";
+            this.buttonKirimChat.Size = new System.Drawing.Size(70, 27);
+            this.buttonKirimChat.TabIndex = 36;
+            this.buttonKirimChat.Text = "Kirim";
+            this.buttonKirimChat.UseVisualStyleBackColor = false;
+            this.buttonKirimChat.Click += new System.EventHandler(this.buttonKirimChat_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Orange;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(476, 49);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "CHAT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonKeluar
+            // 
+            this.buttonKeluar.BackColor = System.Drawing.Color.Orange;
+            this.buttonKeluar.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKeluar.ForeColor = System.Drawing.Color.White;
+            this.buttonKeluar.Location = new System.Drawing.Point(399, 246);
+            this.buttonKeluar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonKeluar.Name = "buttonKeluar";
+            this.buttonKeluar.Size = new System.Drawing.Size(86, 32);
+            this.buttonKeluar.TabIndex = 34;
+            this.buttonKeluar.Text = "&KELUAR";
+            this.buttonKeluar.UseVisualStyleBackColor = false;
+            this.buttonKeluar.Click += new System.EventHandler(this.buttonKeluar_Click);
+            // 
+            // listBoxChat
+            // 
+            this.listBoxChat.BackColor = System.Drawing.Color.White;
+            this.listBoxChat.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxChat.FormattingEnabled = true;
+            this.listBoxChat.ItemHeight = 19;
+            this.listBoxChat.Location = new System.Drawing.Point(7, 63);
+            this.listBoxChat.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxChat.Name = "listBoxChat";
+            this.listBoxChat.Size = new System.Drawing.Size(478, 137);
+            this.listBoxChat.TabIndex = 37;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel1.Controls.Add(this.textBoxChat);
+            this.panel1.Controls.Add(this.buttonKirimChat);
+            this.panel1.Location = new System.Drawing.Point(8, 206);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(477, 35);
+            this.panel1.TabIndex = 38;
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxChat.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxChat.Location = new System.Drawing.Point(5, 8);
+            this.textBoxChat.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.Size = new System.Drawing.Size(396, 21);
+            this.textBoxChat.TabIndex = 0;
+            this.textBoxChat.Text = "Type Here...";
+            this.textBoxChat.Enter += new System.EventHandler(this.textBoxChat_Enter);
+            this.textBoxChat.Leave += new System.EventHandler(this.textBoxChat_Leave);
+            // 
+            // labelSeen
+            // 
+            this.labelSeen.AutoSize = true;
+            this.labelSeen.Location = new System.Drawing.Point(12, 257);
+            this.labelSeen.Name = "labelSeen";
+            this.labelSeen.Size = new System.Drawing.Size(35, 13);
+            this.labelSeen.TabIndex = 39;
+            this.labelSeen.Text = "label2";
+            // 
+            // FormChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 284);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelSeen);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBoxChat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonKeluar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormChat";
+            this.Load += new System.EventHandler(this.FormChat_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonKirimChat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonKeluar;
+        private System.Windows.Forms.ListBox listBoxChat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxChat;
+        private System.Windows.Forms.Label labelSeen;
+    }
+}
